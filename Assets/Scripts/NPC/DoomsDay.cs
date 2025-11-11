de10f84f91592740ef2f9694a5678d73465ece10
김지훈 <kdhkdh181@gmail.com>
2025-11-11 22:20:31 +0900

    doomsday 코드 개선

--- Assets/Scripts/NPC/DoomsDay.cs
+++ Assets/Scripts/NPC/DoomsDay.cs
@@ -7,7 +7,6 @@ public class DoomsDay : MonoBehaviour
 	[Header("DoomsDay State")] // 종말론자 관련 필드. Dommsday는 마지막 날, 종말론 등의 뜻을 가지고 있다.
 	public bool doomsdayRecruited = false; // 종말론자 영입 여부
 	public int doomsdayRecruitDay = 0; // 종말론자 영입하고부터 지난 날짜
-	public bool isReturnedToBunker = false; // 벙커로 복귀 여부
 
 	[Header("Refs")]
 	public TimeOfDaySystem timeSystem; // 시간 시스템
@@ -17,12 +16,16 @@ public class DoomsDay : MonoBehaviour
         timeSystem.onNextDay.AddListener(OnNextDay);
     }
 
+    // 영입 이벤트 처리
+    // HERE
+
     // 다음 날 이벤트 처리
     public void OnNextDay(){
         if(doomsdayRecruited){
             doomsdayRecruitDay++;
             if(doomsdayRecruitDay >= 3){
                 endingManager.DoomsdayEnding();
+                Destroy(gameObject); // 오브젝트 삭제
             }
         }
     }
