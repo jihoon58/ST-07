63f0721c2abcad3b63bbe76cc5a9a5c139b7e17e
김지훈 <kdhkdh181@gmail.com>
2025-12-01 00:28:13 +0900

    불필요한 스크립트 삭제

---
+++ Assets/Scripts/Data/InventoryData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+/// <summary>
+/// 인벤토리 데이터
+/// </summary>
+
+// HERE
+public class InventoryData : MonoBehaviour
+{
+    
+}
---
+++ Assets/Scripts/Data/PlayerData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+/// <summary>
+/// 플레이어 데이터
+/// </summary>
+ 
+// HERE
+public class PlayerData : MonoBehaviour
+{
+    
+}
