ff7c47e9af81dd2bfb5c00473ec5ce03903574b8
dlwjdgh060201 <misosky21c@gmail.com>
2025-12-01 03:09:19 +0900

    자잘한거 복구

--- Assets/Scripts/Player/CamMove.cs
+++ Assets/Scripts/Player/CamMove.cs
@@ -2,23 +2,42 @@ using UnityEngine;
 
 public class CamMove : MonoBehaviour
 {
-    private Vector3 originPos; // 카메라 원래 위치 변수
-    private Vector2 distance; // 인벤 열었을 때의 카메라 이동거리 변수
-    private void Start()
-    {
-        originPos = new Vector3(0, 0, -10); // 카메라 원래 위치 초기화
-        transform.position = originPos; // 카메라 위치를 원래 위치로 설정
+    //private Vector3 originPos; // 카메라 원래 위치 변수
+    //private Vector2 distance; // 인벤 열었을 때의 카메라 이동거리 변수
+    //private void Start()
+    //{
+    //    originPos = new Vector3(0, 0, -10); // 카메라 원래 위치 초기화
+    //    transform.position = originPos; // 카메라 위치를 원래 위치로 설정
 
-        distance = new Vector2(100, 0); // 카메라 이동 거리 초기화
+    //    distance = new Vector2(100, 0); // 카메라 이동 거리 초기화
+    //}
+
+    //public void MoveRIghtCamera()
+    //{
+    //    transform.Translate(distance); // 카메라를 distance만큼 이동
+    //}
+
+    //public void MoveLeftCamera()
+    //{
+    //    transform.position = originPos; // 카메라를 원래 위치로 이동
+    //}
+
+    private Vector3 originLocalPos;
+    private Vector2 distance;
+
+    void Start()
+    {
+        originLocalPos = transform.localPosition;  // 자식 기준 원래 위치 저장
+        distance = new Vector2(5, 0);              // 너무 크니까 5 정도로 줄이기
     }
 
-    public void MoveRIghtCamera()
+    public void MoveRightCamera()
     {
-        transform.Translate(distance); // 카메라를 distance만큼 이동
+        transform.localPosition = originLocalPos + (Vector3)distance;
     }
 
     public void MoveLeftCamera()
     {
-        transform.position = originPos; // 카메라를 원래 위치로 이동
+        transform.localPosition = originLocalPos;
     }
 }
